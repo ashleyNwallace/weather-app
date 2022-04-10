855a47a567a56db528948ba723364e4d1b5e9315
Ashley Wallace <103217263+ashleyNwallace@users.noreply.github.com>
2022-04-10 15:56:32 -0600

    showing the current weather conditions

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
 
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("#currentTemp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let weatherDescription = document.querySelector("#weatherDescription");
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  let windSpeed = document.querySelector("#windSpeed");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  let humidity = document.querySelector("#humidityElement");
+  humidity.innerHTML = response.data.main.humidity;
+}
+
+let apiKey = "5723740d8d0f3d5046687fbab7668982";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
