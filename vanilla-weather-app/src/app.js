992c37d0353c2832d62c22155734a12055bc3919
Ashley Wallace <103217263+ashleyNwallace@users.noreply.github.com>
2022-04-10 16:29:31 -0600

    added date element and added city element

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -1,6 +1,27 @@
-function displayTemperature(response) {
-  console.log(response.data);
+function displayDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hoours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 
+function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
   let temperatureElement = document.querySelector("#currentTemp");
@@ -11,9 +32,12 @@ function displayTemperature(response) {
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
   let humidity = document.querySelector("#humidityElement");
   humidity.innerHTML = response.data.main.humidity;
+  let dateElement = document.querySelector("#dateElement");
+  dateElement.innerHTML = displayDate(response.data.dt * 1000);
 }
 
 let apiKey = "5723740d8d0f3d5046687fbab7668982";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let city = "Aberdeen";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
