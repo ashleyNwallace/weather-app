3ca3855c7bdd1b584eaf32ab71fa9fea5cc3d873
Ashley Wallace <103217263+ashleyNwallace@users.noreply.github.com>
2022-04-10 21:04:00 -0600

    updated search buttono

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -42,8 +42,17 @@ function displayTemperature(response) {
   iconDisplay.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "5723740d8d0f3d5046687fbab7668982";
-let city = "Denver";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "5723740d8d0f3d5046687fbab7668982";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#cityElement");
+  search(cityElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
