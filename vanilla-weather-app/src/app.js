d024b64467cd48be616a4b3805d512400a4b5f35
Ashley Wallace <103217263+ashleyNwallace@users.noreply.github.com>
2022-04-11 15:35:38 -0600

    added function to get daily forecast

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -21,6 +21,12 @@ function displayDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "5723740d8d0f3d5046687fbab7668982";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
@@ -41,6 +47,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconDisplay.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function farenheitTemp(event) {
@@ -55,7 +63,7 @@ function celsiusTemp(event) {
   document.querySelector("#currentTemp").innerHTML = tempC;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
@@ -104,4 +112,3 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Madrid");
-displayForecast();
